b38ee2b33b182c7f2c26f5541424598597407ef2
Victoriya Gorskaya <victoriya.gorskaya@mail.ru>
2023-02-25 21:51:45 +0300

    Initial commit

---
+++ calculator/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home_view(request):
+        return HttpResponse('Рецепты: '
+        '   Омлет: /omlet;   '
+        '   Паста: /pasta;   '
+        '   Бутерброд: /buter')
+
+def omlet(request):
+    servings = int(request.GET.get('servings', 1))
+    context = {
+        'recipe': {
+            'яйца, шт': 2*servings,
+            'молоко, л': 0.1*servings,
+            'соль, ч.л.': 0.5*servings
+        }
+    }
+    return render(request, 'calculator/index.html', context)
+
+def pasta(request):
+    servings = int(request.GET.get('servings', 1))
+    context = {
+        'recipe': {
+            'макароны, г': 0.3*servings,
+            'сыр, г': 0.05*servings
+        }
+    }
+    return render(request, 'calculator/index.html', context)
+
+def buter(request):
+    servings = int(request.GET.get('servings', 1))
+    context = {
+        'recipe': {
+            'хлеб, ломтик': 1*servings,
+            'колбаса, ломтик': 1*servings,
+            'сыр, ломтик': 1*servings,
+            'помидор, ломтик': 1*servings
+        }
+    }
+    return render(request, 'calculator/index.html', context)
